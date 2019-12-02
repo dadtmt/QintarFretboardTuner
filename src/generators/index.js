141e8c96b3963c6585fb06c68672770971022149
Thomas Culdaut <thomas@wildcodeschool.fr>
2019-12-02 23:15:34 +0100

    P5 strings

--- src/generators/index.js
+++ src/generators/index.js
@@ -1,5 +1,6 @@
 import * as R from "ramda"
 import {enharmonic, transposeFrom} from "@tonaljs/note"
+import {transpose} from "@tonaljs/tonal"
 
 const fretIntervals = [
     "m2","M2","m3", "M3","P4","d5","P5","m6","M6","m7","M7","P8"
@@ -7,3 +8,11 @@ const fretIntervals = [
 
 export const generateString = flatNote => 
     R.map(R.pipe(transposeFrom(flatNote), enharmonic), fretIntervals)
+
+export const generateStringsFromDeepestNote = (flatNote, acc = [] ,stringCount = 6) =>
+    stringCount > 0 ? 
+        generateStringsFromDeepestNote(transpose(flatNote,"P5"), [flatNote, ...acc], stringCount - 1)
+        : acc
+
+
+export const generateFretboard = () => null
--- src/generators/index.test.js
+++ src/generators/index.test.js
@@ -1,4 +1,4 @@
-import {generateString} from './index'
+import {generateFretboard, generateString, generateStringsFromDeepestNote} from './index'
 
 it('generates E4 string', () => {
     expect(generateString('e4')).toMatchSnapshot()
@@ -8,3 +8,10 @@ it('generates A3 string', () => {
     expect(generateString('a3')).toMatchSnapshot()
 })
 
+it('generates a P5 StringsFlatNote', () => {
+    expect(generateStringsFromDeepestNote("F1")).toMatchSnapshot()
+})
+
+it('generates a Quart fretboard', () => {
+    expect(generateFretboard()).toMatchSnapshot()
+})

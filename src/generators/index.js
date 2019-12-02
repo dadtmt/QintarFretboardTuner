74e2ae46c57b6acbc7e8ac656290db42417afe82
Thomas Culdaut <thomas@wildcodeschool.fr>
2019-12-02 13:01:06 +0100

    with enharmonic

--- src/generators/index.js
+++ src/generators/index.js
@@ -1,10 +1,9 @@
 import * as R from "ramda"
-import  {transpose} from "@tonaljs/tonal"
+import {enharmonic, transposeFrom} from "@tonaljs/note"
 
 const fretIntervals = [
     "m2","M2","m3", "M3","P4","d5","P5","m6","M6","m7","M7","P8"
 ]
 
 export const generateString = flatNote => 
-    fretIntervals.map(fretInterval => 
-        transpose(flatNote, fretInterval))
+    fretIntervals.map(R.pipe(transposeFrom(flatNote), enharmonic))

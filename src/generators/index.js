a9cb7572535b604af394cc3e25e9be259c46dffd
Thomas Culdaut <thomas@wildcodeschool.fr>
2019-12-02 12:36:59 +0100

    generate string

--- src/generators/index.js
+++ src/generators/index.js
@@ -1,13 +1,10 @@
 import * as R from "ramda"
-import  {distance, interval, transpose} from "@tonaljs/tonal"
+import  {transpose} from "@tonaljs/tonal"
 
-export const addSemitone = note => transpose(note, "m2")
+const fretIntervals = [
+    "m2","M2","m3", "M3","P4","d5","P5","m6","M6","m7","M7","P8"
+]
 
-export const areInSameOctave = (a, b) => 
-    R.prop("oct", interval(distance(a, b))) === 0
-    
-export const generateFret = 
-flatNote => 
-    fret => areInSameOctave(flatNote, fret) && [addSemitone(fret), addSemitone(fret)]
 export const generateString = flatNote => 
-R.unfold(generateFret(flatNote), flatNote)
+    fretIntervals.map(fretInterval => 
+        transpose(flatNote, fretInterval))
--- src/generators/index.test.js
+++ src/generators/index.test.js
@@ -1,32 +1,10 @@
-import {addSemitone, areInSameOctave, generateFret, generateString} from './index'
-
-describe ("areInSameOctave", () => {
-    it("E4 and F4 are in the same octave", () => {
-        expect(areInSameOctave("e4", "f4")).toBeTruthy()
-    })
-    it("F4 and E5 are same octave", () => {
-        expect(areInSameOctave("f4", "e5")).toBeTruthy()
-    })
-    it("E4 and E5 are not in the same octave", () => {
-        expect(areInSameOctave("e4", "e5")).toBeFalsy()
-    })
-    it("E4 and F5 are not the same octave", () => {
-        expect(areInSameOctave("e4", "f5")).toBeFalsy()
-    })
-})
-
-describe ("generateFret", () => {
-    it("returns next semitone", () => {
-        expect(generateFret('E4')('E4')).toEqual(['F4','F4'])
-    })
-    it("returns next semitone", () => {
-        expect(generateFret('E4')('F4')).toEqual(['Gb4', 'Gb4'])
-    })
-    it("returns false when fret is the octave above the flat note", () => {
-        expect(generateFret('E4')('E5')).toBeFalsy()
-    })
-})
+import {generateString} from './index'
 
 it('generates E4 string', () => {
     expect(generateString('e4')).toMatchSnapshot()
 })
+
+it('generates A3 string', () => {
+    expect(generateString('a3')).toMatchSnapshot()
+})
+

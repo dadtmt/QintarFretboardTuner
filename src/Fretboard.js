ebf12e41f7fb0066e8b6852135027dd2e8b8da86
Thomas Culdaut <thomas@wildcodeschool.fr>
2019-12-07 23:21:58 +0100

    setLowestNote value

--- src/Fretboard.js
+++ src/Fretboard.js
@@ -1,27 +1,56 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import { simplify } from '@tonaljs/note'
 import { generateFretboard } from './generators'
 import './Fretboard.css'
 
-function Fretboard () {
-  const fretboard = generateFretboard('F1')
-  return (<main>
-    <ol>
-      {fretboard.map(
-        string => <li key={string.flatNote}>{string.flatNote}</li>
-      )}
-    </ol>
-    <ol>
-      {fretboard.map(
-        string => <li key={string.flatNote}>
-          <ol>
-            {
-              string.frets.map(fret => <li key={fret}>{fret}</li>)
-            }
-          </ol>
-        </li>
-      )}
-    </ol>
-  </main>)
+function Fretboard() {
+  const [inputError, setInputError] = useState(false)
+  const [lowestNote, setLowestNote] = useState('F1')
+  const [fretboard, setFretboard] = useState(generateFretboard('F1'))
+
+  return (
+    <Fragment>
+      <form action="">
+        <label htmlFor="lowest-note">
+          Lowest note:
+          <input
+            type="text"
+            value={lowestNote}
+            onChange={e => {
+              const value = e.target.value
+              const noteValue = simplify(value)
+              if (noteValue) {
+                setInputError(false)
+                setFretboard(generateFretboard(noteValue))
+              } else {
+                setInputError(true)
+              }
+              setLowestNote(value)
+            }}
+          />
+        </label>
+        {inputError && <span>not a valid note</span>}
+      </form>
+      <main>
+        <ol>
+          {fretboard.map(string => (
+            <li key={string.flatNote}>{string.flatNote}</li>
+          ))}
+        </ol>
+        <ol>
+          {fretboard.map(string => (
+            <li key={string.flatNote}>
+              <ol>
+                {string.frets.map(fret => (
+                  <li key={fret}>{fret}</li>
+                ))}
+              </ol>
+            </li>
+          ))}
+        </ol>
+      </main>
+    </Fragment>
+  )
 }
 
 export default Fretboard
--- src/generators/index.js
+++ src/generators/index.js
@@ -3,21 +3,43 @@ import { enharmonic, transposeFrom } from '@tonaljs/note'
 import { transpose } from '@tonaljs/tonal'
 
 const fretIntervals = [
-  'm2', 'M2', 'm3', 'M3', 'P4', 'd5', 'P5', 'm6', 'M6', 'm7', 'M7', 'P8'
+  'm2',
+  'M2',
+  'm3',
+  'M3',
+  'P4',
+  'd5',
+  'P5',
+  'm6',
+  'M6',
+  'm7',
+  'M7',
+  'P8'
 ]
 
 export const generateString = flatNote =>
   R.map(R.pipe(transposeFrom(flatNote), enharmonic), fretIntervals)
 
-export const generateStringsFromDeepestNote = (flatNote, stringCount = 6, acc = []) =>
+export const generateStringsFromDeepestNote = (
+  flatNote,
+  stringCount = 6,
+  acc = []
+) =>
   stringCount > 0
-    ? generateStringsFromDeepestNote(transpose(flatNote, 'P5'), stringCount - 1, [flatNote, ...acc])
+    ? generateStringsFromDeepestNote(
+        transpose(flatNote, 'P5'),
+        stringCount - 1,
+        [flatNote, ...acc]
+      )
     : acc
 
-export const generateFretboard = R.pipe(
-  generateStringsFromDeepestNote,
-  R.map(flatNote => ({
-    flatNote,
-    frets: generateString(flatNote)
-  }))
+export const generateFretboard = R.memoizeWith(
+  R.identity,
+  R.pipe(
+    generateStringsFromDeepestNote,
+    R.map(flatNote => ({
+      flatNote,
+      frets: generateString(flatNote)
+    }))
+  )
 )

5b489f3049e7785e2894ac124654b74f904e5e4d
Thomas Culdaut <thomas@wildcodeschool.fr>
2019-12-14 01:06:27 +0100

    test functionality

--- src/Fretboard.js
+++ src/Fretboard.js
@@ -1,11 +1,14 @@
 import React, { Fragment, useState } from 'react'
 import { simplify } from '@tonaljs/note'
+import { chord } from '@tonaljs/chord'
 import { generateFretboard } from './generators'
 import './Fretboard.css'
 
 function Fretboard() {
   const [noteInputError, setNoteInputError] = useState(false)
   const [deepestNote, setDeepestNote] = useState('F1')
+  const [chordInput, setChordInput] = useState('Cmaj7')
+  const [selectedChord, setSelectedChord] = useState(chord('C3maj7'))
   const [fretboard, setFretboard] = useState(generateFretboard('F1'))
 
   return (
@@ -42,7 +45,10 @@ function Fretboard() {
             <li key={string.flatNote}>
               <ol>
                 {string.frets.map(fret => (
-                  <li key={fret}>{fret}</li>
+                  <li key={fret}>
+                    {fret}
+                    {selectedChord.notes.includes(fret) && 'YOOO'}
+                  </li>
                 ))}
               </ol>
             </li>

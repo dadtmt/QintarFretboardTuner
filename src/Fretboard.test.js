a75e72ea8a5f25529d22fc9bd6c384f5c3d784cc
tmt <thomas.culdaut@gmail.com>
2019-12-14 01:03:35 +0100

    fix style (vscode update problem)

--- src/Fretboard.test.js
+++ src/Fretboard.test.js
@@ -4,24 +4,21 @@ import Fretboard from './Fretboard'
 import renderer, { act } from 'react-test-renderer'
 import { toMatchDiffSnapshot } from 'snapshot-diff'
 
-
 expect.extend({ toMatchDiffSnapshot })
-
 
 it('renders correctly', () => {
   const component = renderer.create(<Fretboard />)
   const tree = component.toJSON()
   expect(tree).toMatchSnapshot()
 
-  const wrongEventValue = { target: { value: "xVfGu" } }
+  const wrongEventValue = { target: { value: 'xVfGu' } }
   act(() => {
     component.root.findAllByType('input')[0].props.onChange(wrongEventValue)
-  });
+  })
 
   const treeUpdate = component.toJSON()
   console.log(treeUpdate)
   expect(tree).toMatchDiffSnapshot(treeUpdate)
-
 })
 
 it('renders without crashing', () => {

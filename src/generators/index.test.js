7fcdb4d23061bf00b4827680a750204017437947
Thomas Culdaut <thomas@wildcodeschool.fr>
2019-12-16 23:47:29 +0100

    organize generators snapshot

--- src/generators/index.test.js
+++ src/generators/index.test.js
@@ -1,25 +1,32 @@
 import {
   generateFretboard,
   generateString,
-  generateStringsFromDeepestNote
+  generateStringsFromDeepestNote,
+  setChordIntervals
 } from './index'
 
-it('generates E4 string', () => {
-  expect(generateString('e4')).toMatchSnapshot()
+describe('generateString', () => {
+  it('generates E4 string', () => {
+    expect(generateString('e4')).toMatchSnapshot()
+  })
+
+  it('generates A3 string', () => {
+    expect(generateString('a3')).toMatchSnapshot()
+  })
 })
 
-it('generates A3 string', () => {
-  expect(generateString('a3')).toMatchSnapshot()
+describe('generateString', () => {
+  it('generates a P5 StringsFlatNote', () => {
+    expect(generateStringsFromDeepestNote('F1')).toMatchSnapshot()
+  })
+
+  it('generates a 10 P5 StringsFlatNote', () => {
+    expect(generateStringsFromDeepestNote('F1', 10)).toMatchSnapshot()
+  })
 })
 
-it('generates a P5 StringsFlatNote', () => {
-  expect(generateStringsFromDeepestNote('F1')).toMatchSnapshot()
-})
-
-it('generates a 10 P5 StringsFlatNote', () => {
-  expect(generateStringsFromDeepestNote('F1', 10)).toMatchSnapshot()
-})
-
-it('generates a Quintar fretboard', () => {
-  expect(generateFretboard('F1')).toMatchSnapshot()
+describe('generateFretboard', () => {
+  it('generates a Quintar fretboard', () => {
+    expect(generateFretboard('F1')).toMatchSnapshot()
+  })
 })

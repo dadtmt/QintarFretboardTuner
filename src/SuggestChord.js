33d2b8882026896c396d185e4e3252d82ea3bb62
Thomas Culdaut <thomas@wildcodeschool.fr>
2019-12-22 12:33:35 +0100

    chordSuggestions is a plain array

--- src/SuggestChord.js
+++ src/SuggestChord.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import { entries } from '@tonaljs/chord-dictionary'
+
+export const chordSuggestions = entries()
+  .map(chordType => chordType.name)
+  .filter(chordType => chordType)
 
 function SuggestChord() {
   return <div>SuggestChord</div>
--- src/SuggestChord.test.js
+++ src/SuggestChord.test.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import SuggestChord from './SuggestChord'
 import renderer, { act } from 'react-test-renderer'
 import { toMatchDiffSnapshot } from 'snapshot-diff'
+import SuggestChord, { chordSuggestions } from './SuggestChord'
 
 expect.extend({ toMatchDiffSnapshot })
 
-it('renders correctly', () => {
-  const component = renderer.create(<SuggestChord />)
-  const tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
+describe('chordSuggestions', () => {
+  it('suggest all chord names', () => {
+    expect(chordSuggestions).toMatchSnapshot()
+  })
 })
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<SuggestChord />, div)
-  ReactDOM.unmountComponentAtNode(div)
+describe('SuggestChord', () => {
+  it('renders correctly', () => {
+    const component = renderer.create(<SuggestChord />)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<SuggestChord />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
 })
